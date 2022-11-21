c0928787e009557dc90d61293afabd9e921a92a0
unknown <tijaniadebayoabdllahi@gmail.com>
2022-11-21 13:27:54 +0100

    usingFile

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LegitBankApp.Menu;
+using LegitBankApp.Model;
+
+namespace LegitBankApp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"\t
+
+###########################################################################
+####>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>####
+####___________________________________________________________________####
+####     ZZZZZZZZZ EEEEEEEE  NN      NN   II  TTTTTTTTTT HH    HH      ####
+####                                                                   ####
+####          ZZ   EE        NN NN   NN   II      TT     HH    HH      ####
+####        ZZ     EEEEEEEE  NN  NN  NN   II      TT     HHHHHHHH      ####
+####      ZZZ      EE        NN   NN NN   II      TT     HH    HH      ####
+####   ZZZZZZZZZ   EEEEEEEE  NN      NN   II      TT     HH    HH      ####
+####___________________________________________________________________####
+####>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>####
+###########################################################################");
+            Console.WriteLine($"\n______________________________________________________________________________\n");
+
+
+
+
+
+            FileHandlingMenu file = new FileHandlingMenu();
+            file.ReloadAllFiles();
+            var viewMenu = new ViewMenu();
+            viewMenu.OverAll();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
